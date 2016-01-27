4343436acf93e0e66535a34479c7aafcfdaab686
Daniel Weinmann <danielweinmann@gmail.com>
2016-01-27 01:03:49 -0200

    Adds specs for user password encryption

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,22 @@
 require "spec_helper"
+require "bcrypt"
 
 RSpec.describe User do
+  describe "#password=" do
+    it "should set encrypted password" do
+      user = User.new
+      user.password = '12345678'
+      assert user.encrypted_password != '12345678'
+      assert BCrypt::Password.new(user.encrypted_password) == '12345678'
+    end
+  end
+
+  describe "#password" do
+    it "should return encrypted password" do
+      user = User.new
+      user.password = '12345678'
+      assert !(user.password === '12345678')
+      assert user.password == '12345678'
+    end
+  end
 end
