4932c3ba331f4a65759236bf6d0e0b71ea0b5fad
Daniel Weinmann <danielweinmann@gmail.com>
2016-03-26 22:09:48 -0300

    Validates that message is from demand or transaction user

--- lib/models/message.rb
+++ lib/models/message.rb
@@ -5,6 +5,12 @@ class Message < Sequel::Model
   many_to_one :transaction
   many_to_one :user
 
+  def validate
+    super
+    return unless self.transaction && self.user
+    errors.add(:user, 'must be participating of the transaction') unless [self.transaction.user, self.transaction.demand.user].include?(self.user)
+  end
+
   def after_create
     super
     self.transaction.update last_message_text: self.text
