7f48a9446b86f0ba38d191e56f93ac8e714b5eef
Daniel Weinmann <danielweinmann@gmail.com>
2016-04-24 22:49:09 -0300

    Adds facebook connect action to users endpoint

--- lib/endpoints/users.rb
+++ lib/endpoints/users.rb
@@ -28,6 +28,23 @@ module Endpoints
         encode serialize(user)
       end
 
+      put "/facebook" do
+        begin
+          url = "https://graph.facebook.com/v2.5/me?fields=id%2Cbio%2Cemail%2Cfirst_name%2Cgender%2Clast_name%2Cname&access_token=#{body_params[:facebook_token]}"
+          facebook = JSON.parse(RestClient.get(url, :content_type => :json, :accept => :json))
+          begin
+            User.where(facebook_uid: facebook['id']).update(facebook_uid: nil)
+            current_user.update(facebook_uid: facebook['id'])
+            encode serialize(current_user, :current_user)
+          rescue
+            status 422
+            errors(user.errors.full_messages)
+          end
+        rescue
+          raise Pliny::Errors::Unauthorized
+        end
+      end
+
       put "/:id" do |id|
         user = User.first(id: id)
         raise Pliny::Errors::NotFound unless user
