ca48187ce8b4d02842410d04d0c3e439ab0e8f29
Daniel Weinmann <danielweinmann@gmail.com>
2016-05-01 22:31:32 -0300

    Adds can_review_ids to Transaction

--- lib/models/transaction.rb
+++ lib/models/transaction.rb
@@ -5,6 +5,7 @@ class Transaction < Sequel::Model
   many_to_one :demand
   many_to_one :user
   one_to_many :messages
+  one_to_many :reviews
 
   def validate
     super
@@ -15,6 +16,19 @@ class Transaction < Sequel::Model
   def after_save
     super
     self.demand.update updated_at: DateTime.now
+  end
+
+  def can_review_ids
+    users = []
+    users << self.user.id if can_review(self.user)
+    users << self.demand.user.id if can_review(self.demand.user)
+    users
+  end
+
+  private
+
+  def can_review(user)
+    self.reviews_dataset.where(reviewer: user).count == 0 && self.messages.size > 0
   end
 
 end
--- lib/serializers/transaction.rb
+++ lib/serializers/transaction.rb
@@ -5,6 +5,7 @@ class Serializers::Transaction < Serializers::Base
       demand: Serializers::Demand.new(:default).serialize(arg.demand),
       user: Serializers::User.new(:default).serialize(arg.user),
       last_message_text: arg.last_message_text,
+      can_review_ids: arg.can_review_ids,
     }
   end
 end
