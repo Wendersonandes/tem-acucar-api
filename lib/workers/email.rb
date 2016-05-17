7df2dc20eefd4a356737ef18b748a4bedd3a14f5
Daniel Weinmann <danielweinmann@gmail.com>
2016-05-17 14:18:19 -0300

    Updates email notification text

--- lib/workers/email.rb
+++ lib/workers/email.rb
@@ -8,10 +8,10 @@ module Workers
       template = SendGrid::Template.new(Config.sendgrid_template_id)
       client = SendGrid::Client.new(api_key: Config.sendgrid_api_key)
       mailer = SendGrid::TemplateMailer.new(client, template, recipients)
-      full_message = "Olá, #{user.first_name}!\n\n#{message}"
+      full_message = "Olá, #{user.first_name}!\n\n#{message}\n\nPara saber mais ou responder, acesse o aplicativo do Tem Açúcar."
       mailer.mail({
         from: 'ola@temacucar.com',
-        from_name: 'Tem Açúcar',
+        from_name: 'Tem Açúcar?',
         html: full_message.gsub("\n", "<br/>"),
         text: full_message,
         subject: subject
